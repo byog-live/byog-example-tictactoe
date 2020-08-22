6cd902f124704db0868588cd7a0762fadbce3488
Po Chen <chenpaul914@gmail.com>
2020-08-22 14:33:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import game from '@byog/sdk';
 import gameDef from './game.js';
 import {setup, draw} from './ui.js';
 
+game.dev(gameDef, true);
 setup(game);
 draw(game);
-
-game.dev(gameDef, true);
