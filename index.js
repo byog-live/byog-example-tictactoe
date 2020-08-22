3b1233e40fe6f1b90c8f60991a9d14d0e4f9a732
Po Chen <chenpaul914@gmail.com>
2020-08-22 14:31:34 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ import game from '@byog/sdk';
 import gameDef from './game.js';
 import {setup, draw} from './ui.js';
 
-game.handlePush = ({event, payload}) => {
-  switch (event) {
-    case 'join':
-      break;
-    default:
-      state[event] = payload;
-      draw();
-  }
-}
-
 setup(game);
 draw(game);
 
